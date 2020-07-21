acb5c2d4c949422d093694e4d19ec3b562d960eb
zekron <zekron@foxmail.com>
2020-07-21 22:16:49 +0800

    AB包测试

---
+++ Assets/Editor/BuildAssetBundle.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using UnityEditor;
+
+
+/// <summary>
+/// 打包脚本 —— 放在 Editor 文件夹下(规范)
+/// </summary>
+public class ChinarAssetBundle
+{
+    [MenuItem("Editor Build/Build AssetsBundle")] //菜单栏添加按钮
+    static void BuildAllAssetsBundles()
+    {
+        string folder = "CalamotoAssetBundles";                                                               //定义文件夹名字
+        if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);                                     //文件夹不存在，则创建
+        BuildPipeline.BuildAssetBundles(folder, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows); //创建AssetBundle
+    }
+}
