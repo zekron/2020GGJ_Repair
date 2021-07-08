d28c3291c5133da78417d80fecfef2692e94b6ba
Zikzik <34153457+zekron@users.noreply.github.com>
2021-07-09 01:23:22 +0800

    Update SpriteAtlasMgr.cs

--- Assets/Scripts/Utils/SpriteAtlasMgr.cs
+++ Assets/Scripts/Utils/SpriteAtlasMgr.cs
@@ -1,22 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.U2D;
-using UnityEngine.U2D;
 
 [CreateAssetMenu(fileName = "New SpriteAtlasManagerSO", menuName = "Scriptable Object/SpriteAtlasManagerSO")]
 public class SpriteAtlasMgr : ScriptableObject
 {
-    [SerializeField] SpriteAtlas[] _winSpriteAtlas;
-    [SerializeField] SpriteAtlas[] _webGLSpriteAtlas;
+    [SerializeField] UnityEngine.U2D.SpriteAtlas[] _winSpriteAtlas, _webGLSpriteAtlas;
 #if UNITY_EDITOR
     private void OnEnable()
     {
 #if UNITY_STANDALONE_WIN
         foreach (var atlas in _webGLSpriteAtlas)
-            SpriteAtlasExtensions.SetIncludeInBuild(atlas, true);
+            UnityEditor.U2D.SpriteAtlasExtensions.SetIncludeInBuild(atlas, true);
         foreach (var atlas in _winSpriteAtlas)
-            SpriteAtlasExtensions.SetIncludeInBuild(atlas, false);
+            UnityEditor.U2D.SpriteAtlasExtensions.SetIncludeInBuild(atlas, false);
 #elif UNITY_WEBGL
         foreach (var atlas in _winSpriteAtlas)
             SpriteAtlasExtensions.SetIncludeInBuild(atlas, true);
