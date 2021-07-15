6f940bebd4ea40a58e7b3aa7a49c83df4d021590
Zikzik <34153457+zekron@users.noreply.github.com>
2021-07-15 17:56:23 +0800

    Update map

--- Assets/Scripts/Character/DestroyDetector.cs
+++ Assets/Scripts/Character/DestroyDetector.cs
@@ -29,7 +29,6 @@ public class DestroyDetector : MonoBehaviour
         }
         if (other.CompareTag("Border"))
         {
-            Debug.Log("aaa");
             CharacterAbilities.instance.RebirthCharacter();
         }
     }
