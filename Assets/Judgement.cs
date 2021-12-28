7793ae685869165b0360d5b552bd6680a103aaf7
Zikzik <34153457+zekron@users.noreply.github.com>
2021-12-28 13:14:47 +0800

    New Test

--- Assets/Judgement.cs
+++ Assets/Judgement.cs
@@ -23,21 +23,15 @@ public class Judgement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.DrawRay(A.position, A.forward, Color.red);
+        Debug.DrawRay(A.position, A.up, Color.red);
         Debug.DrawLine(A.position, B.position, Color.red);
 
         Debug.Log(IsOutOfBounds(A, B));
     }
 
-    bool IsOutOfBounds(Transform aimer, Transform target)
+
+    private void OnDrawGizmosSelected()
     {
-        float distance = Vector3.Distance(aimer.position, target.position);
-        //float dot = Vector3.Dot(aimer.forward, target.position - aimer.position);
-        //float angle = Mathf.Acos(Vector3.Dot(A.right, (B.position - A.position).normalized)) * Mathf.Rad2Deg;
-        float angle = Vector3.Angle(aimer.forward, target.position - aimer.position);
-
-        Vector3 cross = Vector3.Cross(aimer.forward, target.position - aimer.position);
-
         #region Draw Triangles
         vh.Clear();
         float x = 0, z = 0;
@@ -54,7 +48,7 @@ public class Judgement : MonoBehaviour
                 z = Mathf.Cos((i * delta - LeftAngle) * Mathf.Deg2Rad);
                 //Debug.LogFormat("({0}, {1})", x, z);
                 // 添加顶点
-                vh.AddVert(new Vector3(x * Radius, 0, z * Radius), Color.blue, new Vector2(x, z));
+                vh.AddVert(new Vector3(x * Radius, z * Radius, 0), Color.blue, new Vector2(x, z));
             }
             else
             {
@@ -63,7 +57,7 @@ public class Judgement : MonoBehaviour
                 z = Mathf.Cos((i - triangleCount) * delta * Mathf.Deg2Rad);
                 //Debug.LogFormat("({0}, {1})", x, z);
                 // 添加顶点
-                vh.AddVert(new Vector3(x * Radius, 0, z * Radius), Color.red, new Vector2(x, z));
+                vh.AddVert(new Vector3(x * Radius, z * Radius, 0), Color.red, new Vector2(x, z));
             }
             if (i > 0)
             {
@@ -85,15 +79,36 @@ public class Judgement : MonoBehaviour
         renderer.receiveShadows = false;
         #endregion
 
-        if (distance > Radius) return false;
-        if (cross.y > 0)    //right side
+    }
+
+    string IsOutOfBounds(Transform aimer, Transform target)
+    {
+        var AxisX = aimer.up;
+        var AxisY = target.position - aimer.position;
+
+        float distance = Vector3.Distance(aimer.position, target.position);
+        //float dot = Vector3.Dot(aimer.forward, target.position - aimer.position);
+        //float angle = Mathf.Acos(Vector3.Dot(A.right, (B.position - A.position).normalized)) * Mathf.Rad2Deg;
+        float angle = Vector3.Angle(AxisX, AxisY);
+
+        Vector3 AxisZ = Vector3.Cross(AxisX, AxisY);
+
+
+        if (distance > Radius) return "Out of radius";
+        if (AxisZ.z < 0)    //right side
         {
-            if (angle > RightAngle) return false;
+            if (angle > RightAngle) return "Right side, out of bounds";
+            else return "Right side, in bounds";
         }
         else
         {
-            if (angle > LeftAngle) return false;
+            if (angle > LeftAngle) return "Left side, out of bounds";
+            else return "Left side, in bounds";
         }
-        return true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        
     }
 }
